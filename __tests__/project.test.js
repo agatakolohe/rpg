b1585d3aac16e68fee94f15d42c9a155d124baba
agatakolohe <agatakolohe@gmail.com>
2020-11-12 20:47:00 -0800

    player constructor test pass
    Co-authored-by: Jeff Chiu <jwchiu82@gmail.com>

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -5,6 +5,6 @@ describe('Player', () => {
   test('should correctly create player object with name and type', () => {
     const player1 = new Player('Samwise', 'hobbit');
     expect(player1.name).toEqual('Samwise');
-    expect(player1.type).toEqual('elf')
+    expect(player1.type).toEqual('hobbit')
   })
 });
