dde335efac90287a84dfbfff8bafa72d220576bb
agatakolohe <agatakolohe@gmail.com>
2020-11-12 20:44:56 -0800

    create player constructor test good fail
    Co-authored-by: Jeff Chiu <jwchiu82@gmail.com>

---
+++ __tests__/project.test.js
@@ -0,0 +1,10 @@
+import Player from '../src/project.js'; 
+
+describe('Player', () => {
+
+  test('should correctly create player object with name and type', () => {
+    const player1 = new Player('Samwise', 'hobbit');
+    expect(player1.name).toEqual('Samwise');
+    expect(player1.type).toEqual('elf')
+  })
+});
---
+++ src/project.js
@@ -0,0 +1,7 @@
+//Business Logic// functions go here
+export default class Player {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+}
