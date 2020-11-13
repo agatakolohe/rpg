0f9aa2543a880a2fa4db6d7da74b8a8b3d7265de
agatakolohe <agatakolohe@gmail.com>
2020-11-12 21:30:41 -0800

    comment out code for future use
    Co-authored-by: Jeff Chiu <jwchiu82@gmail.com>

--- src/project.js
+++ src/project.js
@@ -4,6 +4,7 @@ export default class Player {
     this.name = name;
     this.type = type;
     this.hunger = 100;
+    this.hitPoints = 100;
     //this.inventory = [];
   }
 
@@ -14,4 +15,18 @@ export default class Player {
       return 'stupid fat hobbit!'
     }
   }
-}
+
+//   exhaustion(effect) {
+//     if (effect = 
+//     }
+//   }
+// }
+// Character.prototype.heal = function(healing){   //  PLAYER HEALING FUNCTION
+//   if(this.hp + healing > 100){
+//     this.hp = 100;
+//   }
+//   else{
+//     this.hp += healing;
+//   }
+//   game.displayAll();
+// };
